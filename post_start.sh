139c86f26e36678a6d1291e239624c073a101a34
op131csharpminr <op131csharpminr>
2024-10-25 13:59:44 -0600

    Removing comment

--- post_start.sh
+++ post_start.sh
@@ -7,6 +7,3 @@ echo "export POETRY_HOME=/workspace/opt/poetry" >> ~/.bashrc
 echo "export POETRY_CACHE_DIR=/workspace/opt/poetry/cache" >> ~/.bashrc
 echo "export CARGO_HOME=/workspace/opt/cargo" >> ~/.bashrc
 echo "export PATH=$PATH:/workspace/opt/poetry/bin:/workspace/opt/cargo/bin" >> ~/.bashrc
-
-# Log in to Hugging Face
-# cat /workspace/hf_token | tr -d " \t\n\r" | xargs -0 -I {} poetry run huggingface-cli login --token={}
